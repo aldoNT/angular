3217db0379a559bd3a3e9c26d2f01ad61f8311ba
thierry navas <thierry.navas3@free.fr>
2016-01-11 14:59:28 +0100

    Etape 1.6.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 (function(){
     var nta= {nom: 'NAVAS', prenom:'Thierry'};
     var gem = { name: 'Azurite', price: 2.95, descriptif:'C\'est le descriptif du produit', canPurchase: true,
-    soldOut: true};
+    soldOut: false};
   var app = angular.module('gemStore', []);
   app.controller('NomController', function(){
     this.lesnoms = nta;
